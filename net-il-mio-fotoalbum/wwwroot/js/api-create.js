12f96dcc781d80b92c9b75953fb70eb7e20ddebf
andreadldev <andrea.delisi2@gmail.com>
2023-04-26 18:45:28 +0200

    form contatti

--- net-il-mio-fotoalbum/wwwroot/js/api-create.js
+++ net-il-mio-fotoalbum/wwwroot/js/api-create.js
@@ -1,20 +1,20 @@
 ﻿
-const postPost = post => axios
-    .post("/api/message", post)
+const postMessage = message => axios
+    .post("/api/message", message)
     .then(() => location.href = "/photo/apiindex");
 
 const initCreateForm = () => {
-    const form = document.querySelector("#post-create-form");
+    const form = document.querySelector("#message-create-form");
 
     form.addEventListener("submit", e => {
         e.preventDefault();
 
-        const post = getPostFromForm(form);
-        postPost(post);
+        const message = getMessageFromForm(form);
+        postMessage(message);
     });
 };
 
-const getPostFromForm = form => {
+const getMessageFromForm = form => {
     const email = form.querySelector("#email").value;
     const text = form.querySelector("#text").value;
 
